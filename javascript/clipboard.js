b8c55594bea96a2704f771531d9948a465826c3a
Tea Condemns <83653555+TeaCondemns@users.noreply.github.com>
2021-05-07 22:24:57 +0300

    Create clipboard.js

---
+++ javascript/clipboard.js
@@ -0,0 +1,12 @@
+var copy = (str) => {
+    const el = document.createElement('textarea');
+    el.value = str;
+    el.setAttribute('readonly', '');
+    el.style.position = 'absolute';
+    el.style.left = '-9999px';
+    document.body.appendChild(el);
+    el.select();
+    el.setSelectionRange(0, 99999);
+    document.execCommand('copy');
+    document.body.removeChild(el);
+};
