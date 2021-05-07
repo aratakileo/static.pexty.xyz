4f69c01cdf0a946df1c82d1c5eb4d0460de721c4
Tea Condemns <83653555+TeaCondemns@users.noreply.github.com>
2021-05-07 21:00:23 +0300

    Create getparams.js

---
+++ javascript/getparams.js
@@ -0,0 +1,19 @@
+var getparams = () => {
+  var returnedParams = {};
+  var url = window.location.search.substring(1); //get rid of "?" in querystring
+  var qArray = url.split('&'); //get key-value pairs
+  for (var i = 0; i < qArray.length; i++) 
+  {
+    var pArr = qArray[i].split('='); //split key and value
+    var value = decodeURI(pArr[1]);
+    
+    if (!isNaN(value)) value -= 0;
+    if (value == 'true' || value == 'True') value = true;
+    if (value == 'false' || value == 'False') value = false;
+    if (value == 'null' || value == 'None') value = null;
+    
+    returnedParams[pArr[0]] = value;
+  }
+        
+  return returnedParams;
+};
