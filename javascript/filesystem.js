6fe84375b57d0aca658a6d1c1f959dae2d0c31c6
Tea Condemns <83653555+TeaCondemns@users.noreply.github.com>
2021-05-07 22:52:59 +0300

    Create filesystem.js

---
+++ javascript/filesystem.js
@@ -0,0 +1,10 @@
+function open(filePath) {
+  var result = null;
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("GET", filePath, false);
+  xmlhttp.send();
+  if (xmlhttp.status==200) {
+    result = xmlhttp.responseText;
+  }
+  return result;
+}
